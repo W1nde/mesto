e2ad0791269ca47eec925efe2c0ac9b97ae56f24
W1nde <markshadpalov@gmail.com>
2021-11-19 23:42:47 +0700

    main

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,15 @@ const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const popupClose = document.querySelector(".popup__close");
 const form = document.querySelector(".popup__form");
-const nameForm = document.querySelector(".popup__subtitle_name");
-const jobForm = document.querySelector(".popup__subtitle_job");
-const popupSave = document.querySelector(".popup__save-button");
+const nameForm = document.querySelector(".popup__subtitle__type_name");
+const jobForm = document.querySelector(".popup__subtitle__type_job");
+const popupSave = document.querySelector(".popup__save");
 const userName = document.querySelector(".profile__name");
 const userJob = document.querySelector(".profile__job")
 
 function open() {
+    userName.textContent = nameForm.value;
+    userJob.textContent = jobForm.value;
     popup.classList.add("popup_opened");
 }
 
@@ -26,8 +28,3 @@ function edit (evt) {
 editButton.addEventListener('click', open);
 popupClose.addEventListener('click', close);
 form.addEventListener('submit', edit);
-
-console.log(nameForm.value)
-console.log(jobForm.value)
-console.log(userName.textContent)
-console.log(userJob.textContent)
