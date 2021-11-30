0dad1c543c35576f1f3ae1dd09c7c62cf8d3194c
W1nde <markshadpalov@gmail.com>
2021-11-30 21:02:43 +0700

    main

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,66 @@
 const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
-const popupClose = document.querySelector(".popup__close");
 const form = document.querySelector(".popup__form");
 const nameForm = document.querySelector(".popup__input_type_name");
 const jobForm = document.querySelector(".popup__input_type_job");
 const popupSave = document.querySelector(".popup__save");
 const userName = document.querySelector(".profile__name");
-const userJob = document.querySelector(".profile__job")
+const userJob = document.querySelector(".profile__job");
 
-function open() {
-    nameForm.value = userName.textContent;
-    jobForm.value = userJob.textContent;
-    popup.classList.add("popup_opened");
+
+const addButton = document.querySelector(".profile__add-button")
+const popupEdit = document.querySelector(".popup__window_type_edit");
+const popupAdd = document.querySelector(".popup__window_type_add");
+const formEdit = document.querySelector(".popup__form_type_edit");
+const formAdd = document.querySelector(".popup__form_type_add");
+const pictureForm = document.querySelector(".popup__input_type_pic-name");
+const urlForm = document.querySelector(".popup__input_type_url");
+
+const closeBtnPopupEdit = popupEdit.querySelector('.popup__close');
+const closeBtnPopupAdd = popupAdd.querySelector('.popup__close');
+
+const likeButton = document.querySelectorAll('element__like-button')
+
+function like() {
+    likeButton.classList.toggle("element__like-button_active")
 }
 
-function close() {
-    popup.classList.remove("popup_opened");
+
+function closeEditPopup() {
+    popupEdit.classList.remove("popup_opened");
+}
+
+function closeAddPopup() {
+    popupAdd.classList.remove("popup_opened");
+}
+
+function openEditPopup() {
+    nameForm.value = userName.textContent;
+    jobForm.value = userJob.textContent;
+    popupEdit.classList.add("popup_opened");
+}
+
+function openAddPopup() {
+    popupAdd.classList.add("popup_opened");
 }
 
 function edit (evt) {
     evt.preventDefault();
     userName.textContent = nameForm.value;
     userJob.textContent = jobForm.value;
-    close();
+    closeEditPopup();
 } 
 
-editButton.addEventListener('click', open);
-popupClose.addEventListener('click', close);
-form.addEventListener('submit', edit);
+function add (evt) {
+    evt.preventDefault();
+    popupAdd.classList.remove("popup_opened");
+    closeAddPopup();
+}
+
+editButton.addEventListener('click', openEditPopup);
+addButton.addEventListener('click', openAddPopup);
+closeBtnPopupEdit.addEventListener('click', closeEditPopup);
+closeBtnPopupAdd.addEventListener('click', closeAddPopup);
+formEdit.addEventListener('submit', edit);
+formAdd.addEventListener('submit', add);
+likeButton.addEventListener('click', like);
