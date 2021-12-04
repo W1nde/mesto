8cac173747797e146b432be809bdd766b80904ec
W1nde <markshadpalov@gmail.com>
2021-12-04 22:11:00 +0700

    main

---
+++ scripts/index.js
@@ -0,0 +1,118 @@
+const inputName = document.querySelector(".popup__input_type_name");
+const inputJob = document.querySelector(".popup__input_type_job");
+const userName = document.querySelector(".profile__name");
+const userJob = document.querySelector(".profile__job");
+const elements = document.querySelector(".elements");
+const popupEditProfile = document.querySelector(".popup_type_edit");
+const popupAddContent = document.querySelector(".popup_type_add");
+const popupPic = document.querySelector(".popup_type_pic");
+const formEdit = document.querySelector(".popup__form_type_edit");
+const formAdd = document.querySelector(".popup__form_type_add");
+const inputPic = document.querySelector(".popup__input_type_pic-name");
+const inputUrl = document.querySelector(".popup__input_type_url");
+const closeBtnPopupEditProfile = popupEditProfile.querySelector(".popup__close");
+const closeBtnPopupAddContent = popupAddContent.querySelector(".popup__close");
+const closeBtnPopupPic = popupPic.querySelector(".popup__close");
+const likeButton = document.querySelector(".element__like-button");
+const addContentBtn = document.querySelector(".profile__add-button");
+const editProfileBtn = document.querySelector(".profile__edit-button");
+const popupImage = document.querySelector(".popup__pic");
+const popupPicTitle = document.querySelector(".popup__pic-title");
+const popup = document.querySelector(".popup");
+
+function getElement(el) {
+  const cardTemplate = document.querySelector("#cardTemplate").content;
+  const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
+  const cardPicture = cardElement.querySelector(".element__image");
+  cardElement.querySelector(".element__title").textContent = el.name;
+  cardPicture.src = el.link;
+  cardPicture.alt = el.name;
+  const delButton = cardElement.querySelector(".element__trash");
+  delButton.addEventListener("click", handleDelete);
+  const likeButton = cardElement.querySelector(".element__like-button");
+  likeButton.addEventListener("click", handleLike);
+  cardPicture.addEventListener("click", handleImgClick);
+  return cardElement;
+}
+
+function render() {
+  const htmlCards = initialCards.map((el) => {
+    return getElement(el);
+  });
+  elements.append(...htmlCards);
+}
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+function formProfileSubmitHandler(evt) {
+  evt.preventDefault();
+  userName.textContent = inputName.value;
+  userJob.textContent = inputJob.value;
+  closePopup(popupEditProfile);
+}
+
+function formPlaceSubmitHandler(evt) {
+  evt.preventDefault();
+  const inputs = {
+    name: inputPic.value,
+    link: inputUrl.value,
+  };
+  const newElement = getElement(inputs);
+  elements.prepend(newElement);
+  closePopup(popupAddContent);
+}
+
+render();
+function handleDelete(event) {
+  const eventTarget = event.target;
+  const element = eventTarget.closest(".element");
+  element.remove();
+}
+
+function handleLike(event) {
+  const eventTarget = event.target;
+  eventTarget.classList.toggle("element__like-button_active");
+}
+
+function handleImgClick(event) {
+  const eventTarget = event.target;
+  const element = eventTarget.closest(".element");
+  popupPicTitle.textContent = element.textContent;
+  popupImage.src = element.querySelector(".element__image").src;
+  popupImage.alt = element.textContent;
+  openPopup(popupPic);
+}
+
+function openPopupEditProfile() {
+  openPopup(popupEditProfile);
+  inputName.value = userName.textContent;
+  inputJob.value = userJob.textContent;
+}
+
+function openPopupAddContent() {
+  openPopup(popupAddContent);
+  inputUrl.value = "";
+  inputPic.value = "";
+}
+
+function closePopupEditProfile() {
+  closePopup(popupEditProfile);
+}
+
+function closePicPopup() {
+  closePopup(popupPic);
+}
+
+editProfileBtn.addEventListener("click", openPopupEditProfile);
+addContentBtn.addEventListener("click", openPopupAddContent);
+closeBtnPopupEditProfile.addEventListener('click', closePopupEditProfile);
+closeBtnPopupAddContent.addEventListener("click", closePopup(popupAddContent));
+closeBtnPopupPic.addEventListener("click", closePicPopup);
+formAdd.addEventListener("submit", formPlaceSubmitHandler);
+formEdit.addEventListener("submit", formProfileSubmitHandler);
